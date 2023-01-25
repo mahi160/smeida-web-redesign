f75d02eadd44afe5cd09ac4926fae49d03fa3698
Salauddin Omar Sifat <omarsifat288@gmail.com>
2023-01-25 10:07:27 +0600

    product part done

--- src/routes/ui/product-card.svelte
+++ src/routes/ui/product-card.svelte
@@ -1,18 +1,25 @@
 <script lang="ts">
-		export let imgSrc: string;
+	export let imgSrc: string;
 	export let title: string;
 	export let description: string;
+	export let buttonHeader: string;
+	export let path: string;
 </script>
 
 <div
-	class="flex flex-col gap-3 p-5 rounded cursor-pointer ring-2 ring-neutral-500 hover:ring-primary-400"
+	class="flex flex-col gap-3 p-5 rounded cursor-pointer ring-2 ring-neutral-500 dark:ring-neutral-700"
 >
-	<div
-		class="flex items-center justify-center px-4 pt-5 pb-5 border-b border-gray-200 border-none sm:px-6"
-	>
+	<div class="flex items-center justify-center pt-2">
 		<img class="w-full max-w-xs aspect-video" src={imgSrc} alt={title} />
 	</div>
 
-	<h3 class="text-xl font-semibold text-text-800">{title}</h3>
-	<p class="text-text-500">{description}</p>
+	<h3 class="text-xl font-semibold">{title}</h3>
+	<p class="text-text-500 dark:text-text-400">{description}</p>
+	<a href={path} class="flex items-center justify-center w-full mt-3">
+		<button
+			class="px-3 py-2 text-sm font-semibold text-white rounded bg-accent-500 hover:bg-accent-400"
+		>
+			{buttonHeader}
+		</button>
+	</a>
 </div>
--- src/routes/ui/product.svelte
+++ src/routes/ui/product.svelte
@@ -3,46 +3,59 @@
 
 	const products = [
 		{
-			imgSrc: '/assets/home/traffic.svg',
+			// imgSrc: '/assets/home/traffic.svg',
+			imgSrc: '/assets/home/features_1.png',
 			title: 'Understand Traffic',
 			description:
-				'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit amet voluptatum possimus est similique odio quos mollitia corporis facere commodi quas.'
+				'By showing the right car to the right buyer, we increase the number of engaged prospects who come to your VDPs.',
+			buttonHeader: 'Maximize Engagement',
+			path: ''
 		},
 		{
-			imgSrc: '/assets/home/popup.svg',
+			imgSrc: '/assets/home/features_2.png',
+			// imgSrc: '/assets/home/popup.svg',
 			title: 'Increase Quality Leads',
 			description:
-				'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit amet voluptatum possimus est similique odio quos mollitia corporis facere commodi quas.'
+				'By employing tools like Smart Offer you can increase the likelihood of a lead conversion even further.',
+			buttonHeader: 'Optimize Digital Shopping',
+			path: ''
 		},
 		{
-			imgSrc: '/assets/home/grow.svg',
+			imgSrc: '/assets/home/features_3.png',
+			// imgSrc: '/assets/home/grow.svg',
 			title: 'Grow Sales',
 			description:
-				'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit amet voluptatum possimus est similique odio quos mollitia corporis facere commodi quas.'
+				'Our services generate more high-quality VDP traffic per dollar than any other advertising channels',
+			buttonHeader: 'Lorem, ipsum.',
+			path: ''
 		}
 	];
 </script>
 
-<section class="flex flex-col pt-5">
-	<h2
-		class="max-w-lg pb-6 text-3xl font-semibold text-text-800 md:text-4xl lg:text-5xl sm:max-w-xl md:max-w-3xl lg:max-w-4xl"
-	>
-		We Help You Understand
-		<span class="text-transparent bg-clip-text bg-gradient-to-r from-primary-500 to-primary-400">
-			Your Business
-		</span>
-	</h2>
-	<p class="text-lg text-text-500 xl:text-xl 2xl:text-2xl sm:max-w-xl md:max-w-3xl lg:max-w-6xl">
-		Lorem ipsum dolor sit amet consectetur adipisicing elit. Id odit asperiores atque harum nemo
-		reiciendis distinctio incidunt enim ut itaque.
-		<strong>
-			Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore, delectus.
-		</strong>
-	</p>
-</section>
+<section class="container px-5 pb-10 mx-auto sm:p-20 sm:pt-0">
+	<section class="flex flex-col pt-5">
+		<h2
+			class="max-w-lg pb-6 text-3xl font-semibold md:text-4xl lg:text-5xl sm:max-w-xl md:max-w-3xl lg:max-w-4xl"
+		>
+			We Help You Understand
+			<span class="text-transparent bg-clip-text bg-gradient-to-r from-primary-500 to-primary-500">
+				Your Business
+			</span>
+		</h2>
+		<p
+			class="text-lg text-text-500 dark:text-text-400 xl:text-xl 2xl:text-2xl sm:max-w-xl md:max-w-3xl lg:max-w-6xl"
+		>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Id odit asperiores atque harum nemo
+			reiciendis distinctio incidunt enim ut itaque.
+			<strong>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore, delectus.
+			</strong>
+		</p>
+	</section>
 
-<section class="grid grid-cols-1 gap-10 mt-10 md:grid-cols-3">
-	{#each products as product}
-		<ProductCard {...product} />
-	{/each}
+	<section class="grid grid-cols-1 gap-10 mt-10 md:grid-cols-3">
+		{#each products as product}
+			<ProductCard {...product} />
+		{/each}
+	</section>
 </section>
