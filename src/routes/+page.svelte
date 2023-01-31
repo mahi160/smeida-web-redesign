a4dc107de5dabfe15f4008074b59e64b0aa3bc4b
Salauddin Omar Sifat <omarsifat288@gmail.com>
2023-01-31 13:02:03 +0600

    fix space in title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <svelte:head>
-	<title>sMedia| Top Digital Tech Company for Automotive</title>
+	<title>sMedia | Top Digital Tech Company for Automotive</title>
 </svelte:head>
 
 <Hero />
